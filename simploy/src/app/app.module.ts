aaad54e2829371e11b27e4b5887a9be437eb4944
Ihsan Taha <ihsan_taha@yahoo.com>
2017-12-20 19:12:20 -0800

    Adding new version of simploy folder without submodule

---
+++ simploy/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
