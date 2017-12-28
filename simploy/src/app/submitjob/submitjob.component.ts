ae690954fce2687a5c8c4c91424c416ddf30f365
Morgan Worthington <morgan.worthington@gmail.com>
2017-12-28 15:10:46 -0500

    made a navbar along with page components, have to add tags to pages

---
+++ simploy/src/app/submitjob/submitjob.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submitjob',
+  templateUrl: './submitjob.component.html',
+  styleUrls: ['./submitjob.component.css']
+})
+export class SubmitjobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
