c51fd06dcec7963871f9efac80c1ed0a9a2d6fff
Trent Smith <smithtrent00@gmail.com>
2017-12-31 06:27:03 -0800

    Adding view jobs with show job/resume (for now) per table row

--- simploy/src/app/viewjobs/viewjobs.component.ts
+++ simploy/src/app/viewjobs/viewjobs.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-viewjobs',
   templateUrl: './viewjobs.component.html',
   styleUrls: ['./viewjobs.component.css']
 })
-export class ViewjobsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ViewjobsComponent implements OnInit 
+{
+  tableData:any[];
+  viewResume:boolean=false;
+  constructor(private http:HttpClient) { }
+  Email:any;
+  firstname:any;
+  lastname:any;
+  skill:any[];
+  ngOnInit() 
+  {
+    this.getTableData();
   }
-
+  getTableData()
+  {
+    this.http.get("http://localhost:3000/users")
+    .subscribe(
+      (data:any[])=>
+      {
+          this.tableData =data;
+          console.log(this.tableData[0].skills);
+      }
+    )
+  }
+  getUserData(email:any,firstname:any,lastname:any,skills:any)
+  {
+    this.viewResume=true;
+    this.Email = email;
+    this.firstname=firstname;
+    this.lastname=lastname;
+    this.skill=skills.split(" ");
+  }
 }
