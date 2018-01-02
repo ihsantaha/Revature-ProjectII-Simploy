8f7e7136209933b0a9a9a5bba8ca22aef94099ce
Ihsan Taha <ihsan_taha@yahoo.com>
2018-01-02 10:55:09 -0800

    Adding validation to profile

--- simploy/src/app/profile/profile.component.ts
+++ simploy/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../user';
 import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -8,6 +9,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  @ViewChild('profileF') profileF: NgForm;
+  validInput: Boolean;
 
   user: User = JSON.parse(localStorage.getItem('user'));
 
@@ -18,30 +21,34 @@ export class ProfileComponent implements OnInit {
   }
 
   updateUser(e) {
-    e.preventDefault();
 
-    let EMAIL = e.target.elements[0].value;
-    let FIRSTNAME = e.target.elements[1].value;
-    let LASTNAME = e.target.elements[2].value;
-    let PASSWORD = e.target.elements[3].value;
-    let PNUMBER = e.target.elements[4].value;
+    if (this.profileF.form.valid) {
+      e.preventDefault();
 
-    let json = {
-      firstName: FIRSTNAME,
-      lastName: LASTNAME,
-      email: EMAIL,
-      pnumber: PNUMBER,
-      password: PASSWORD,
-      role: this.user.role,
-      id: this.user.id
-    };
-    console.log(JSON.stringify(json));
-    this.httpClient.post('http://localhost:8088/User/update', json).subscribe(
-      (data: any) => {
-        localStorage.setItem('user', JSON.stringify(data));
-        this.user = data;
-      }
-    );
+      let EMAIL = e.target.elements[0].value;
+      let FIRSTNAME = e.target.elements[1].value;
+      let LASTNAME = e.target.elements[2].value;
+      let PASSWORD = e.target.elements[3].value;
+      let PNUMBER = e.target.elements[4].value;
+
+      let json = {
+        firstName: FIRSTNAME,
+        lastName: LASTNAME,
+        email: EMAIL,
+        pnumber: PNUMBER,
+        password: PASSWORD,
+        role: this.user.role,
+        id: this.user.id
+      };
+      console.log(JSON.stringify(json));
+      this.httpClient.post('http://localhost:8088/User/update', json).subscribe(
+        (data: any) => {
+          localStorage.setItem('user', JSON.stringify(data));
+          this.user = data;
+        }
+      );
+
+    }
 
   }
 
