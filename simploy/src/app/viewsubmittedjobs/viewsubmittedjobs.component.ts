754af65ca2886ad1ed99f5d13bd77882ae2e400b
Morgan Worthington <morgan.worthington@gmail.com>
2018-01-02 20:19:09 -0500

    1 step forward, 3 steps back

--- simploy/src/app/viewsubmittedjobs/viewsubmittedjobs.component.ts
+++ simploy/src/app/viewsubmittedjobs/viewsubmittedjobs.component.ts
@@ -1,3 +1,4 @@
+import { LoginService } from './../login.service';
 import { JobTable } from './../JobTable';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -13,6 +14,7 @@ export class ViewsubmittedjobsComponent implements OnInit {
 
   viewResume:boolean=false;
   viewJob:boolean = false;
+  user: User = JSON.parse(localStorage.getItem('user'));
 
   id:number;
   email:any;
@@ -21,55 +23,55 @@ export class ViewsubmittedjobsComponent implements OnInit {
   phone:any;
   skills:any[];
   role:number;
-  tableHold: JobTable[] = [];
+  tableHold: JobTable[] = JSON.parse(localStorage.getItem('Jobs'));
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient,
+              private loginService: LoginService) { }
 
-  ngOnInit() 
-  {
-    this.getTableData();
-  }
+  ngOnInit() {}
 
-  getTableData()
-  {
-    let user: User = JSON.parse(localStorage.getItem('user'));
-    let json = {
-      id: user.id
-    };
-    this.httpClient.post('http://localhost:8088/Job/uidjobs', json)
-    .subscribe(
-      (data: Job[] ) =>
-      {
-        for (var i = 0; i < data.length; i++){
-          let tableData:JobTable= new JobTable;
-          tableData.company=data[i].company;
-          tableData.jobId=data[i].jobId;
-          tableData.description=data[i].description;
-          tableData.postDate=data[i].postDate;
-          tableData.title=data[i].title;
-          tableData.user=data[i].user;
-          tableData.location=data[i].location;
-          if(data[i].skills.length > 0 ){
-            tableData.skills = '';
-          for (var j = 0; j < data[i].skills.length; j++){
-            tableData.skills += data[i].skills[j].title + " "
-          }}
-          //console.log(tableData);
-          this.tableHold[i] = tableData;
-        }
-      }
-    )
-  }
+  // getTableData()
+  // {
+  //   let user: User = JSON.parse(localStorage.getItem('user'));
+  //   let json = {
+  //     id: user.id
+  //   };
+  //   this.httpClient.post('http://localhost:8088/Job/uidjobs', json)
+  //   .subscribe(
+  //     (data: Job[] ) =>
+  //     {
+  //       for (var i = 0; i < data.length; i++){
+  //         let tableData:JobTable= new JobTable;
+  //         tableData.company=data[i].company;
+  //         tableData.jobId=data[i].jobId;
+  //         tableData.description=data[i].description;
+  //         tableData.postDate=data[i].postDate;
+  //         tableData.title=data[i].title;
+  //         tableData.user=data[i].user;
+  //         tableData.location=data[i].location;
+  //         if(data[i].skills.length > 0 ){
+  //           tableData.skills = '';
+  //         for (var j = 0; j < data[i].skills.length; j++){
+  //           tableData.skills += data[i].skills[j].title + " "
+  //         }}
+  //         //console.log(tableData);
+  //         this.tableHold[i] = tableData;
+  //       }
+  //     }
+  //   )
+  // }
 
   delete(id) {
-    // let json = {
-    //   jobId : id
-    // };
-    // this.httpClient.post('localhost:8088/Job/delete', json ).subscribe(
-    //   (data: any[]) => {
-    //     console.log('job deleted');
-    //   }
-    // );
+    let json = {
+      jobId : id
+    };
+    this.httpClient.post('localhost:8088/Job/delete', json ).subscribe(
+      (data: any[]) => {
+        console.log('job deleted');
+      }
+    );
+    this.loginService.getTableDataJobPoster();
+    this.tableHold = JSON.parse(localStorage.getItem('Jobs'));
   }
 
 }
